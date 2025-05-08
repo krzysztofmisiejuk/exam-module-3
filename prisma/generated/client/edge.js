27413bbab25dac4bf9888b9850562a4ee0e9d760
Krzysiek <kmisiejuk15@gmail.com>
2025-05-08 20:49:04 +0200

    change scripts

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -224,11 +224,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_puBhYbQ90lkU@ep-proud-mud-a2v9j0zw-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+        "value": null
       }
     }
   },
